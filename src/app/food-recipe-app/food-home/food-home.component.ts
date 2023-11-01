b2db5662f7837880d2d6071b146b386569b61a7e
--replace-all <paragunhale1998@gmail.com>
2023-11-01 22:14:09 +0530

    new Task

--- src/app/food-recipe-app/food-home/food-home.component.ts
+++ src/app/food-recipe-app/food-home/food-home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { RecipiApiService } from 'src/app/services/recipi-api.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-food-home',
   templateUrl: './food-home.component.html',
   styleUrls: ['./food-home.component.scss']
 })
-export class FoodHomeComponent {
+export class FoodHomeComponent implements OnInit{
+
+  allRecipies:any =[]
+  constructor(private recipiService:RecipiApiService,private sanitizer: DomSanitizer){}
+  
+  ngOnInit(): void {
+    
+    this.getRecipiDetails('')
+}
+getRecipiDetails(recipiName:any){
+  this.recipiService.getRecipi(recipiName).subscribe({
+    next : (data:any) => {
+      this.allRecipies = data.meals
+      console.log(this.allRecipies)
+    },error(err) {
+      console.log(err)
+    },
+  })
+}
+
+getVideoUrl(youtubeUrl: string): SafeResourceUrl {
+  // Extract the video ID from the YouTube URL
+  const videoId = youtubeUrl.split('v=')[1];
+  const sanitizedUrl = `https://www.youtube.com/embed/${videoId}`;
+  return this.sanitizer.bypassSecurityTrustResourceUrl(sanitizedUrl);
+}
 
 }
--- src/app/services/recipi-api.service.ts
+++ src/app/services/recipi-api.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipiApiService {
-
-  constructor() { }
+  private url: string = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+  constructor(private http: HttpClient) {}
+  getRecipi(name: string) {
+    return this.http.get(this.url + name);
+  }
 }
